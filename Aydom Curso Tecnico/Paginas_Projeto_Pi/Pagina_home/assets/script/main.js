8f1fd5f9204e621bcb147a83946db1a58ef9aab8
AydomSantos <aydomaparecido@gmail.com>
2024-11-06 21:15:48 -0300

    Adcionando a seção dos carros

--- Aydom Curso Tecnico/Paginas_Projeto_Pi/Pagina_home/assets/script/main.js
+++ Aydom Curso Tecnico/Paginas_Projeto_Pi/Pagina_home/assets/script/main.js
@@ -1,23 +1,36 @@
 // Configuração do Swiper principal (com navegação)
+// Este Swiper é usado para navegar entre os slides com os botões de navegação
 var swiper = new Swiper(".mySwiper", {
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    // Botões de navegação
+    nextEl: ".swiper-button-next",  
+    prevEl: ".swiper-button-prev",  
   },
   pagination: {
-    el: ".swiper-pagination-main",  // Seletor exclusivo para o swiper principal
-    clickable: true,
+    // Configuração da paginação para o swiper principal
+    el: ".swiper-pagination-main",  
+    clickable: true, 
   },
 });
 
-
-// Configuração do Swiper das últimas buscas (com visualização de 3 slides)
+// Configuração do Swiper para as últimas buscas (com visualização de 5 slides)
 var swiperUltimasBuscas = new Swiper(".section__ultimas__buscas--swiper", {
-  slidesPerView: 5,
-  spaceBetween: 2,
-  freeMode: true,
+  slidesPerView: 5,  
+  spaceBetween: 3,  
+  freeMode: true,  
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: ".swiper-pagination",  
+    clickable: true,  
+  },
+});
+
+// Configuração do Swiper para "Mais Procurados em Carros" (com visualização de 5 slides)
+var swiperCarros = new Swiper(".section__mais__procurados__em__carros--swiper", {
+  slidesPerView: 5,  
+  spaceBetween: 3,  
+  freeMode: true,  
+  pagination: {
+    el: ".swiper-pagination",  
+    clickable: true,  
   },
 });
